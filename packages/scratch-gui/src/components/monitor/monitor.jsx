201bcc61e273c4abda0b4d4342446885b9cd59e8
Bogomil-Stoyanov <bogi2208@gmail.com>
2025-02-05 09:38:53 +0200

    feat: [UEPR-56] Context menu in monitor.jsx

--- packages/scratch-gui/src/components/monitor/monitor.jsx
+++ packages/scratch-gui/src/components/monitor/monitor.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import Draggable from 'react-draggable';
 import {FormattedMessage} from 'react-intl';
-import {ContextMenu, ContextMenuItem, ContextMenuContent, ContextMenuTrigger, ContextMenuPortal} from '@radix-ui/react-context-menu';
+import * as ContextMenu from '@radix-ui/react-context-menu';
 import Box from '../box/box.jsx';
 import DefaultMonitor from './default-monitor.jsx';
 import LargeMonitor from './large-monitor.jsx';
 import SliderMonitor from '../../containers/slider-monitor.jsx';
 import ListMonitor from '../../containers/list-monitor.jsx';
 import {getColorsForTheme} from '../../lib/themes/index.js';
+import contextMenuStyles from '../context-menu/context-menu.css';
+import { MenuItem, BorderedMenuItem } from '../context-menu/context-menu.jsx';
 
 import styles from './monitor.css';
+
 
 // Map category name to color name used in scratch-blocks Blockly.Colours
 const categoryColorMap = {
@@ -39,102 +41,107 @@ const getCategoryColor = (theme, category) => {
     };
 };
 
-const MonitorComponent = props => (
-    <ContextMenu>
-        <ContextMenuTrigger asChild>
-            <Draggable
-                bounds=".monitor-overlay" // Class for monitor container
-                cancel=".no-drag" // Class used for slider input to prevent drag
-                defaultClassNameDragging={styles.dragging}
-                disabled={!props.draggable}
-                onStop={props.onDragEnd}
+const MonitorComponent = (props) => (
+    <ContextMenu.Root>
+        <Draggable
+            bounds=".monitor-overlay"
+            cancel=".no-drag"
+            defaultClassNameDragging={styles.dragging}
+            disabled={!props.draggable}
+            onStop={props.onDragEnd}
+        >
+            <Box
+                className={styles.monitorContainer}
+                componentRef={props.componentRef}
+                onDoubleClick={
+                    props.mode === "list" || !props.draggable
+                        ? null
+                        : props.onNextMode
+                }
             >
-                <Box
-                    className={styles.monitorContainer}
-                    componentRef={props.componentRef}
-                    onDoubleClick={props.mode === 'list' || !props.draggable ? null : props.onNextMode}
-                >
+                <ContextMenu.Trigger className="ContextMenuTrigger">
                     {React.createElement(modes[props.mode], {
-                        categoryColor: getCategoryColor(props.theme, props.category),
-                        ...props
+                        categoryColor: getCategoryColor(
+                            props.theme,
+                            props.category
+                        ),
+                        ...props,
                     })}
-                </Box>
-            </Draggable>
-        </ContextMenuTrigger>
-        {ReactDOM.createPortal((
-            <ContextMenuPortal>
-                <ContextMenuContent
-                    align="start"
-                    sideOffset={5}
-                    className={styles.contextMenu}
-                >
-                    {props.onSetModeToDefault && (
-                        <ContextMenuItem onClick={props.onSetModeToDefault}>
-                            <FormattedMessage
-                                defaultMessage="normal readout"
-                                description="Menu item to switch to the default monitor"
-                                id="gui.monitor.contextMenu.default"
-                            />
-                        </ContextMenuItem>
-                    )}
-                    {props.onSetModeToLarge && (
-                        <ContextMenuItem onClick={props.onSetModeToLarge}>
-                            <FormattedMessage
-                                defaultMessage="large readout"
-                                description="Menu item to switch to the large monitor"
-                                id="gui.monitor.contextMenu.large"
-                            />
-                        </ContextMenuItem>
-                    )}
-                    {props.onSetModeToSlider && (
-                        <ContextMenuItem onClick={props.onSetModeToSlider}>
-                            <FormattedMessage
-                                defaultMessage="slider"
-                                description="Menu item to switch to the slider monitor"
-                                id="gui.monitor.contextMenu.slider"
-                            />
-                        </ContextMenuItem>
-                    )}
-                    {props.onSliderPromptOpen && props.mode === 'slider' && (
-                        <ContextMenuItem onClick={props.onSliderPromptOpen}>
-                            <FormattedMessage
-                                defaultMessage="change slider range"
-                                description="Menu item to change the slider range"
-                                id="gui.monitor.contextMenu.sliderRange"
-                            />
-                        </ContextMenuItem>
-                    )}
-                    {props.onImport && (
-                        <ContextMenuItem onClick={props.onImport}>
-                            <FormattedMessage
-                                defaultMessage="import"
-                                description="Menu item to import into list monitors"
-                                id="gui.monitor.contextMenu.import"
-                            />
-                        </ContextMenuItem>
-                    )}
-                    {props.onExport && (
-                        <ContextMenuItem onClick={props.onExport}>
-                            <FormattedMessage
-                                defaultMessage="export"
-                                description="Menu item to export from list monitors"
-                                id="gui.monitor.contextMenu.export"
-                            />
-                        </ContextMenuItem>
-                    )}
-                    {props.onHide && (
-                        <ContextMenuItem onClick={props.onHide}>
-                            <FormattedMessage
-                                defaultMessage="hide"
-                                description="Menu item to hide the monitor"
-                                id="gui.monitor.contextMenu.hide"
-                            />
-                        </ContextMenuItem>
-                    )}
-                </ContextMenuContent>
-            </ContextMenuPortal>
-        ), document.body)}
-    </ContextMenu>
+                </ContextMenu.Trigger>
+            </Box>
+        </Draggable>
+        <ContextMenu.Content
+            className={contextMenuStyles.contextMenuContent}
+            style={{zIndex: 1000}}
+            avoidCollisions={true}
+            collisionPadding={10}
+            sticky="always"
+        >
+            {props.onSetModeToDefault && (
+                <MenuItem onSelect={props.onSetModeToDefault}>
+                    <FormattedMessage
+                        defaultMessage="normal readout"
+                        description="Menu item to switch to the default monitor"
+                        id="gui.monitor.contextMenu.default"
+                    />
+                </MenuItem>
+            )}
+            {props.onSetModeToLarge && (
+                <MenuItem onSelect={props.onSetModeToLarge}>
+                    <FormattedMessage
+                        defaultMessage="large readout"
+                        description="Menu item to switch to the large monitor"
+                        id="gui.monitor.contextMenu.large"
+                    />
+                </MenuItem>
+            )}
+            {props.onSetModeToSlider && (
+                <MenuItem onSelect={props.onSetModeToSlider}>
+                    <FormattedMessage
+                        defaultMessage="slider"
+                        description="Menu item to switch to the slider monitor"
+                        id="gui.monitor.contextMenu.slider"
+                    />
+                </MenuItem>
+            )}
+            {props.onSliderPromptOpen && props.mode === "slider" && (
+                <BorderedMenuItem onSelect={props.onSliderPromptOpen}>
+                    <FormattedMessage
+                        defaultMessage="change slider range"
+                        description="Menu item to change the slider range"
+                        id="gui.monitor.contextMenu.sliderRange"
+                    />
+                </BorderedMenuItem>
+            )}
+            {props.onImport && (
+                <MenuItem onSelect={props.onImport}>
+                    <FormattedMessage
+                        defaultMessage="import"
+                        description="Menu item to import into list monitors"
+                        id="gui.monitor.contextMenu.import"
+                    />
+                </MenuItem>
+            )}
+            {props.onExport && (
+                <MenuItem onSelect={props.onExport}>
+                    <FormattedMessage
+                        defaultMessage="export"
+                        description="Menu item to export from list monitors"
+                        id="gui.monitor.contextMenu.export"
+                    />
+                </MenuItem>
+            )}
+            {props.onHide && (
+                <MenuItem onSelect={props.onHide}>
+                    <FormattedMessage
+                        defaultMessage="hide"
+                        description="Menu item to hide the monitor"
+                        id="gui.monitor.contextMenu.hide"
+                    />
+                </MenuItem>
+            )}
+        </ContextMenu.Content>
+    </ContextMenu.Root>
 );
 
 const monitorModes = Object.keys(modes);
