e4faf5148fd10c9976d4c4e971f17233fa52db2c
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-09-09 07:53:47 -0700

    test: adjust project execution tests for tap >=18
    Calling `t.plan()` has more significant consequences now in terms of
    test teardown. In particular, calling `t.plan(0)` is a bit hard to deal
    with correctly. Adding 1 to the plan count reported by the project, and
    consistently adding exactly one test after the project finishes, makes
    the behavior pretty much the same as it was with tap 16. I also used
    `async` and `await` to make some of this code easier to read.

--- packages/scratch-vm/test/integration/execute.js
+++ packages/scratch-vm/test/integration/execute.js
@@ -64,7 +64,7 @@ const fileFilter = /\.sb[23]?$/i;
 fs.readdirSync(executeDir)
     .filter(uri => fileFilter.test(uri))
     .forEach(uri => {
-        test(uri, t => {
+        test(uri, async t => {
             // Disable logging during this test.
             log.suggest.deny('vm', 'error');
             t.teardown(() => log.suggest.clear());
@@ -86,13 +86,20 @@ fs.readdirSync(executeDir)
                     t.fail(reason);
                 },
                 plan (count) {
+                    if (didPlan) {
+                        t.fail('tried to set plan more than once');
+                    }
                     didPlan = true;
-                    t.plan(Number(count));
+                    // +1 for t.ok(didEnd) below
+                    t.plan(Number(count) + 1);
                 },
                 end () {
+                    if (didEnd) {
+                        t.fail('tried to end more than once');
+                    }
                     didEnd = true;
                     vm.quit();
-                    t.end();
+                    // don't t.end() here: wait for the t.ok(didEnd) below
                 }
             };
             const reportVmResult = text => {
@@ -128,24 +135,23 @@ fs.readdirSync(executeDir)
 
             // Load the project and once all threads are complete ensure that
             // the scratch project sent us a "end" message.
-            return vm.loadProject(project)
-                .then(() => vm.greenFlag())
-                .then(() => whenThreadsComplete(t, vm, uri))
-                .then(() => {
-                    // Setting a plan is not required but is a good idea.
-                    if (!didPlan) {
-                        t.comment('did not say "plan NUMBER_OF_TESTS"');
-                    }
+            await vm.loadProject(project);
+            vm.greenFlag();
+            await whenThreadsComplete(t, vm, uri);
 
-                    // End must be called so that tap knows the test is done. If
-                    // the test has an SAY "end" block but that block did not
-                    // execute, this explicit failure will raise that issue so
-                    // it can be resolved.
-                    if (!didEnd) {
-                        t.fail('did not say "end"');
-                        vm.quit();
-                        t.end();
-                    }
-                });
+            // Setting a plan is not required but is a good idea.
+            if (!didPlan) {
+                t.comment('did not say "plan NUMBER_OF_TESTS"');
+            }
+
+            // End must be called so that tap knows the test is done. If
+            // the test has an SAY "end" block but that block did not
+            // execute, this explicit failure will raise that issue so
+            // it can be resolved.
+            if (!didEnd) {
+                vm.quit();
+            }
+            t.ok(didEnd, 'did say "end"');
+            t.end();
         });
     });
