3e629988f274313027b8eee487842ae40cc9a3a3
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2025-11-04 10:52:43 -0800

    refactor(deps): externalize dependencies

--- packages/task-herder/vite.config.js
+++ packages/task-herder/vite.config.js
@@ -1,4 +1,15 @@
 import { defineConfig } from 'vite'
+import packageJson from './package.json'
+
+// Externalize all dependencies, peerDependencies, and optionalDependencies, but not devDependencies.
+// Inspired in part by `davidmyersdev/vite-plugin-externalize-deps`.
+// Note that recent versions of Vite automatically externalize Node built-in modules.
+// Adding Node built-ins here suppresses warnings, but I say we want those warnings...
+const externalDeps = [
+  ...Object.keys(packageJson.dependencies || {}),
+  ...Object.keys(packageJson.peerDependencies || {}),
+  ...Object.keys(packageJson.optionalDependencies || {}),
+].map(name => new RegExp(`^${name}(?:/.*)?$`))
 
 export default defineConfig({
   build: {
@@ -6,6 +17,24 @@ export default defineConfig({
       entry: 'src/index.ts',
       name: 'TaskHerder',
       fileName: 'task-herder',
+    },
+    rolldownOptions: {
+      external: externalDeps,
+      output: {
+        /**
+         * Customize the global variable names for externalized dependencies in UMD builds.
+         * @param {string} depName - The name of a dependency
+         * @returns {string} - The global variable name to use for the dependency
+         */
+        globals: depName => {
+          switch (depName) {
+            case 'p-limit':
+              return 'pLimit'
+            default:
+              return depName
+          }
+        },
+      },
     },
   },
 })
